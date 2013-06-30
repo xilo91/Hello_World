388e6bccedee4b9b301b75ecb8dd34dfc0ab75fc
xilo91 <hubert.odziemczyk@gmail.com>
2013-06-30 14:59:33 +0200

    Second

---
+++ src/xilo91/Hello_World.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xilo91;
+
+/**
+ *
+ * @author Hubert Odziemczyk
+ */
+public class Hello_World {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello_World!");
+    }
+}
