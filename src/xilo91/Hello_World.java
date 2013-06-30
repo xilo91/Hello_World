1428a2bd61584544c953cadacda47c9f601284ec
xilo91 <hubert.odziemczyk@gmail.com>
2013-06-30 15:15:02 +0200

    added nice msg!

--- src/xilo91/Hello_World.java
+++ src/xilo91/Hello_World.java
@@ -15,5 +15,6 @@ public class Hello_World {
      */
     public static void main(String[] args) {
         System.out.println("Hello_World!");
+        System.out.println("Branchhyyy !");
     }
 }
